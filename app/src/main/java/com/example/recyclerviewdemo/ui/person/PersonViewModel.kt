b86aaa5f7ae21384a13da0786e255caba7fdf216
jclement92 <christopherjclement@gmail.com>
2021-10-28 23:40:35 -0400

    ViewModel updates
    See the previous commit description for a better description of this commit.

--- app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonViewModel.kt
+++ app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonViewModel.kt
@@ -6,10 +6,6 @@ import com.example.recyclerviewdemo.data.repository.PersonRepository
 import com.example.recyclerviewdemo.data.model.Person
 
 class PersonViewModel : ViewModel() {
-    private val _personList = MutableLiveData<List<Person>>()
+    private val _personList = MutableLiveData(PersonRepository.getPeople())
     val personList get() = _personList
-
-    init {
-        _personList.value = PersonRepository.getPeople()
-    }
 }
