934e024a8d69fa464f74630fca7a37ea71182d60
jclement92 <christopherjclement@gmail.com>
2021-10-28 23:38:57 -0400

    Improve ViewModel class
    Mutable data in the ViewModel should always be private. By using a backing property, other classes can only retrieve (not modify) mutable data from the view model.

--- app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonViewModel.kt
+++ app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonViewModel.kt
@@ -6,9 +6,10 @@ import com.example.recyclerviewdemo.data.repository.PersonRepository
 import com.example.recyclerviewdemo.data.model.Person
 
 class PersonViewModel : ViewModel() {
-    val personList = MutableLiveData<List<Person>>()
+    private val _personList = MutableLiveData<List<Person>>()
+    val personList get() = _personList
 
     init {
-        personList.value = PersonRepository.getPeople()
+        _personList.value = PersonRepository.getPeople()
     }
 }
