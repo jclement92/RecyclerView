516a66210485d71e4fc6f4b03562feec3382b8a1
jclement92 <christopherjclement@gmail.com>
2021-10-28 23:41:59 -0400

    Update viewModel setup syntax
    This is the most up-to-date way of setting up a view model in a fragment as of 2021

--- app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonFragment.kt
+++ app/src/main/java/com/example/recyclerviewdemo/ui/person/PersonFragment.kt
@@ -6,13 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewdemo.databinding.PersonFragmentBinding
 
 class PersonFragment : Fragment() {
     private lateinit var binding: PersonFragmentBinding
-    private lateinit var viewModel: PersonViewModel
+    private val viewModel: PersonViewModel by viewModels()
     private lateinit var personAdapter: PersonAdapter
 
     override fun onCreateView(
@@ -20,7 +21,6 @@ class PersonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = PersonFragmentBinding.inflate(inflater, container, false)
-        viewModel = ViewModelProvider(this).get(PersonViewModel::class.java)
         return binding.root
     }
 
